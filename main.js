ad896ff6ec3e7f174f2cbbc7b7f3a202ea9d8f21
ajitukenori <138645897+ajitukenori@users.noreply.github.com>
2024-02-13 11:25:40 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+    $(".pri-item").slick({
+    });
+});
+
+$(document).ready(function () {
+    $(".ori-item").slick({
+        
+    });
+});
+
+$(document).ready(function () {
+    $(".hd-slide-info").slick({
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows: true,
+    });
+});
